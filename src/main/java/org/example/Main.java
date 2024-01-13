b45971d5a070180a1fb83b71df6cd2d876245968
Krystian <krystian.lewandowski@teb.edu.pl>
2024-01-13 16:21:52 +0100

    39_zadanie24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,70 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        List<Pracownik> listaPraconicy = new ArrayList<>();
+        List<Kierownik> listaKierownicy = new ArrayList<>();
+        Scanner scan = new Scanner(System.in);
 
-        List<Pracownik> praconicy = new ArrayList<>();
-        List<Kierownik> kierownicy = new ArrayList<>();
 
-        Pracownik pracownika1 = new Pracownik( "Anna",
-                                            "Nowak",
-                                        5000,
-                                                    Dzial.IT);
-        Kierownik kierownika1 = new Kierownik( "Adam",
-                                            "Kowal",
-                                        5000,
-                                                    Dzial.IT,
-                                      3000);
 
-        pracownika1.wypiszDane();
-        kierownika1.wypiszDane();
+        while(true) {
+            System.out.println("Wybierz z Menu [wpisz cyfrę:]");
+            System.out.println("1. Dodaj nowego Pracownika.\n" +
+                    "2. Dodaj nowego kierownika.\n" +
+                    "3. Wypisz pracowników i kierowników.\n" +
+                    "4. Zakończ działanie.\n");
+            int wybranaOpcja = scan.nextInt();
+            switch (wybranaOpcja) {
+                case 1 -> {
+                    System.out.println("Podaj imię pracownika: ");
+                    String imie = scan.next();
+                    System.out.println("Podaj nazwisko pracownika: ");
+                    String nazwisko = scan.next();
+                    System.out.println("Podaj wynagrodzenie pracownika: ");
+                    int wynagrodzenie = scan.nextInt();
+                    System.out.println("Podaj dział pracownika: [ IT, KSIEGOWOSC, BOK, BOS ]");
+                    String dzialString = scan.next();
+                    Dzial dzial = Dzial.IT;
+                    if (dzialString.equals(Dzial.IT.toString())) dzial = Dzial.IT;
+                    else if (dzialString.equals(Dzial.KSIEGOWOSC)) dzial = Dzial.KSIEGOWOSC;
+                    else if (dzialString.equals(Dzial.BOK)) dzial = Dzial.BOK;
+                    else if (dzialString.equals(Dzial.BOS)) dzial = Dzial.BOS;
+                    listaPraconicy.add(new Pracownik(imie, nazwisko, wynagrodzenie, dzial));
+                }
+                case 2 -> {
+                    System.out.println("Podaj imię kierownika: ");
+                    String imie = scan.next();
+                    System.out.println("Podaj nazwisko kierownika: ");
+                    String nazwisko = scan.next();
+                    System.out.println("Podaj wynagrodzenie kierownika: ");
+                    int wynagrodzenie = scan.nextInt();
+                    System.out.println("Podaj dział kierownika: [ IT, KSIEGOWOSC, BOK, BOS ]");
+                    String dzialString = scan.next();
+                    Dzial dzial = Dzial.IT;
+                    if (dzialString.equals(Dzial.IT.toString())) dzial = Dzial.IT;
+                    else if (dzialString.equals(Dzial.KSIEGOWOSC)) dzial = Dzial.KSIEGOWOSC;
+                    else if (dzialString.equals(Dzial.BOK)) dzial = Dzial.BOK;
+                    else if (dzialString.equals(Dzial.BOS)) dzial = Dzial.BOS;
+                    System.out.println("Podaj premię kierownika: ");
+                    int premia = scan.nextInt();
+
+                    listaKierownicy.add(new Kierownik(imie, nazwisko, wynagrodzenie, dzial, premia));
+                }
+                case 3 -> {
+                    for (Pracownik pracownik : listaPraconicy) {
+                        pracownik.wypiszDane();
+                    }
+                    for (Kierownik kierownik : listaKierownicy) {
+                        kierownik.wypiszDane();
+                    }
+                }
+            }
+            if(wybranaOpcja == 4) break;
+        }
+
+
+
+
 
     }
 }
