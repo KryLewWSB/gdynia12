408a92b33af66f8a76c9dc7fc86742d38f94e1a3
Krystian <krystian.lewandowski@teb.edu.pl>
2024-01-13 15:28:08 +0100

    34_zadanie24

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -8,27 +8,8 @@ import java.util.regex.Pattern;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner przyjmowacz = new Scanner(System.in);
-
-
-        while(true) {
-            System.out.println("Podaj liczbę 1: [zatwierdź enter]");
-            String liczba1 = przyjmowacz.next();
-            System.out.println("Podaj liczbę 2: [zatwierdź enter]");
-            int liczba2 = przyjmowacz.nextInt();
-            System.out.println("Podaj operator (+, -, *, /), x - zakończy działanie: [zatwierdź enter]");
-            String operator = przyjmowacz.next();
-            switch (operator) {
-                case "+" -> System.out.println("Wynik: " + liczba1 + " + " + liczba2 + " = " + (Integer.parseInt(liczba1) + liczba2));
-                case "-" -> System.out.println("Wynik: " + liczba1 + " - " + liczba2 + " = " + (Integer.parseInt(liczba1) - liczba2));
-                case "*" -> System.out.println("Wynik: " + liczba1 + " * " + liczba2 + " = " + (Integer.parseInt(liczba1) * liczba2));
-                case "/" -> System.out.println("Wynik: " + liczba1 + " / " + liczba2 + " = " + (Integer.parseInt(liczba1) / liczba2));
-                default -> System.out.println("Podałeś operator: " + operator + " podaj poprawny.");
-            }
-            if (operator.equals("x") || operator.equals("X") || operator.equals("exit")) break;
-
-        }
-
+        Pracownik pracownika1 = new Pracownik("Anna", "Nowak", 5000);
+        System.out.println(pracownika1.imie + pracownika1.nazwisko + pracownika1.wynagrodzenie);
 
 
     }
---
+++ src/main/java/org/example/Pracownik.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Pracownik {
+    public String imie;
+    public String nazwisko;
+    public Integer wynagrodzenie;
+
+    public Pracownik(String imie, String nazwisko, Integer wynagrodzenie) {
+        this.imie = imie;
+        this.nazwisko = nazwisko;
+        this.wynagrodzenie = wynagrodzenie;
+    }
+}
